1174f05085e020356b730ec557cad75012b87554
Maximilian Schwarzmüller <business@academind.com>
2017-07-21 07:38:23 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule
